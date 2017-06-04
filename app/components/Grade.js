7c321428e83920b8da06e7bf514f46b6c6f40adb
rockyferrer <rocky.ferrer@mail.utoronto.ca>
2017-06-04 16:46:26 -0400

    GradeForm entries being registered by Grade component

--- app/components/Grade.js
+++ app/components/Grade.js
@@ -4,11 +4,20 @@ import GradeForm from 'GradeForm';
 import GradeMessage from 'GradeMessage';
 
 export default class Grade extends Component {
+    constructor(props) {
+        super(props);
+        this.handleGradeCalculation.bind(this);
+    }
+
+    handleGradeCalculation(grades, weights) {
+        console.log('handleGradeCalculation was reached');
+    }
+
     render() {
         return (
             <div>
                 <p>Grade Component</p>
-                <GradeForm />
+                <GradeForm onGradeSubmit={this.handleGradeCalculation}/>
                 <GradeMessage />
             </div>
         )
--- app/components/GradeForm.js
+++ app/components/GradeForm.js
@@ -1,25 +1,31 @@
 import React, {Component} from 'react';
 
-import GradeEntry from 'GradeEntry';
-
 export default class GradeForm extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            numEntries: 2
-        };
+        this.state = {numEntries: 4};
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    parseGrade(grade) {
+        var divideIndex = grade.indexOf('/');
+        if (divideIndex > -1) {
+            var numerator = parseFloat(grade.substring(0, divideIndex));
+            var denominator = parseFloat(grade.substring(divideIndex+1, grade.length));
+            return ((numerator / denominator) * 100).toFixed(2);
+        } else {
+            return parseFloat(grade);
+        }
     }
 
     handleSubmit(e) {
         e.preventDefault();
+        let gradeElements = [].slice.call(document.getElementsByClassName('grade'));
+        let weightElements = [].slice.call(document.getElementsByClassName('weight'));
+        var grades = gradeElements.map((grade) => this.parseGrade(grade.value));
+        var weights = weightElements.map((weight) => parseFloat(weight.value));
 
-        var grade = parseInt(this.refs.grade.value);
-        var weight = parseInt(this.refs.weight.value);
-        if (!isNaN(grade) && !isNaN(weight)) {
-            console.log('Grade: ' + grade + ' Weight: ' + weight);
-        } else {
-            console.log('Invalid inputs')
-        }
+        this.props.onGradeSubmit(grades, weights);
     }
 
     render() {
@@ -28,16 +34,15 @@ export default class GradeForm extends Component {
             for (let i = 0; i < this.state.numEntries; i++) {
                 allEntries.push(
                     <div key={i}>
-                        <input type="text" ref="grade" placeholder="Enter grade..."/>
-                        <input type="text" ref="weight" placeholder="Enter weight..."/>
+                        <input type="text" className="grade" placeholder="Enter grade..." />
+                        <input type="text" className="weight" placeholder="Enter weight..." />
                     </div>
                 );
             }
             return allEntries;
         }
         return (
-            <form onSubmit={this.handleSubmit.bind(this)}>
-                <p>GradeForm Component</p>
+            <form onSubmit={this.handleSubmit}>
                 {renderEntries()}
                 <button className="button">Submit</button>
             </form>
